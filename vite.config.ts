2c5f5375bc74818021c34b7793071eb28b39c266
MOZ <mozefoka@gmail.com>
2026-09-03 16:41:35 +0300

    IThub-College created

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "styles/variables" as *;`,
+        loadPaths: [path.resolve(__dirname, 'src')],
+      },
+    },
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

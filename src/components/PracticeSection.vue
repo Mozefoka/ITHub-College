d193ade8248b7b6ff8c1942c84bf853d33189bc3
MOZ <mozefoka@gmail.com>
2026-09-09 16:02:37 +0300

    PracticeSection.vue done

--- src/components/PracticeSection.vue
+++ src/components/PracticeSection.vue
@@ -58,14 +58,15 @@ const videos: Media[] = [
   <section class="practice">
     <div class="container">
       <div class="practice__grid">
-        <h2 class="practice__title title">Все говорят <span>о практике</span>,
-          а мы её показываем</h2>
+        <h2 class="practice__title title">
+          Все говорят <span>о практике</span>, а мы её показываем
+        </h2>
 
         <div class="practice__cases">
           <p class="practice__cases-heading">Смотри кейсы кафедр</p>
 
-          <ul class="practice__cases-list">
-            <li v-for="caseItem in cases" class="practice__cases-list-item" :key="caseItem.id">
+          <ul class="practice__cases-tabs">
+            <li v-for="caseItem in cases" class="practice__cases-tab" :key="caseItem.id">
               <a href="#">{{ caseItem.name }}</a>
             </li>
           </ul>
@@ -90,20 +91,26 @@ const videos: Media[] = [
 
 <style scoped lang="scss">
 .practice {
-  padding-bottom: 100vh;
+  padding-bottom: 80px;
 
   &__grid {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 80px 20px;
+
+    @media (max-width: 959px) {
+      grid-template-columns: 1fr;
+      gap: 30px 20px;
+    }
   }
 
   &__title {
-
-  }
-
-  &__cases {
-    grid-column: 1;
+    @media (max-width: 959px) {
+      max-width: 485px;
+      margin: 0 auto;
+      font-size: clamp(22px, 4vw, 32px);
+      text-align: center;
+    }
   }
 
   &__cases-heading {
@@ -113,18 +120,44 @@ const videos: Media[] = [
     margin-bottom: 20px;
   }
 
-  &__cases-list {
+  &__cases-tabs {
     display: flex;
     flex-direction: column;
     gap: 10px;
+    white-space: nowrap;
+
+    @media (max-width: 959px) {
+      flex-direction: row;
+    }
+
+    @media (max-width: 644px) {
+      gap: 5px;
+    }
+
+      @media (max-width: 479px) {
+        flex-wrap: wrap;
+        gap: 15px;
+      }
   }
 
-  &__cases-list-item {
+  &__cases-tab {
     width: fit-content;
     position: relative;
     font-size: 12px;
     border-radius: 15px;
     background-color: $color-purple;
+
+    @media (max-width: 769px) {
+      font-size: 10px;
+    }
+
+    @media (max-width: 644px) {
+      font-size: 8px;
+    }
+
+    @media (max-width: 479px) {
+      font-size: 12px;
+    }
 
     a {
       padding: 10px 35px 10px 15px;
@@ -137,7 +170,22 @@ const videos: Media[] = [
         top: 50%;
         transform: translateY(-50%);
         padding-left: 30px;
-        background: url("@/assets/icons/arrow_right.svg") no-repeat center / 15px;
+        background: url('@/assets/icons/arrow_right.svg') no-repeat center / 15px;
+
+        @media (max-width: 644px) {
+          top: 55%;
+          transform: translateY(-55%);
+          padding-left: 15px;
+          background: url('@/assets/icons/arrow_right.svg') no-repeat center / 10px;
+        }
+      }
+
+      @media (max-width: 769px) {
+        padding: 10px 30px 10px 10px;
+      }
+
+      @media (max-width: 644px) {
+        padding: 10px 20px 10px 10px;
       }
     }
 
@@ -161,18 +209,43 @@ const videos: Media[] = [
     display: flex;
     justify-content: space-between;
     gap: 20px;
+
+    @media (max-width: 959px) {
+      grid-column: 1;
+      grid-row: 2;
+      justify-content: center;
+    }
+
+    @media (max-width: 479px) {
+      flex-direction: column;
+      align-items: center;
+    }
   }
 
   &__video-item {
     max-width: 255px;
     border-radius: 15px;
 
-      @media (max-width: 1299px) {
-        max-width: 225px;
-      }
+    @media (max-width: 1299px) {
+      max-width: 225px;
+    }
 
     @media (max-width: 1099px) {
       max-width: 190px;
+    }
+
+    @media (max-width: 959px) {
+      max-width: 32%;
+      height: 485px;
+    }
+
+    @media (max-width: 679px) {
+      height: 300px;
+    }
+
+    @media (max-width: 479px) {
+      max-width: 100%;
+      height: 485px;
     }
   }
 }

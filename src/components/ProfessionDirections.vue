b0b5345f2c6dc0d4ab8acd342b22c1246c112460
MOZ <mozefoka@gmail.com>
2026-09-13 14:20:21 +0300

    ProfessionDirections.vue done (i hope)

--- src/components/ProfessionDirections.vue
+++ src/components/ProfessionDirections.vue
@@ -53,12 +53,24 @@ import ButtonCta from '@/components/ui/ButtonCta.vue'
   &__content {
     position: relative;
     height: 560px;
+
+      @media (max-width: 679px) {
+        height: 450px;
+      }
+
+      @media (max-width: 539px) {
+        height: 290px;
+      }
   }
 
   &__directions {
     position: absolute;
     inset: 0;
     z-index: 1;
+
+      @media (max-width: 539px) {
+        background: radial-gradient(circle, rgba(168, 85, 247, 0.55) 30%, rgba(168, 85, 247, 0) 50%);
+      }
   }
 
   &__directions-item {
@@ -73,24 +85,88 @@ import ButtonCta from '@/components/ui/ButtonCta.vue'
         width: 120px;
       }
 
+      @media (max-width: 679px) {
+        width: 90px;
+      }
+
+      @media (max-width: 539px) {
+        width: 65px;
+      }
+
     &--first {
-      transform: translate(calc(-50% - 260px), calc(-50% + 50px)) rotate(-50deg);
+      transform: translate(calc(-50% - 300px), calc(-50% + 80px)) rotate(-50deg);
+
+        @media (max-width: 824px) {
+          transform: translate(calc(-50% - 230px), calc(-50% + 50px)) rotate(-50deg);
+        }
+
+        @media (max-width: 679px) {
+          transform: translate(calc(-50% - 210px), calc(-50% + 50px)) rotate(-50deg);
+        }
+
+        @media (max-width: 539px) {
+          transform: translate(calc(-50% - 155px), calc(-50% + 50px)) rotate(-50deg);
+        }
     }
 
     &--second {
-      transform: translate(calc(-50% - 150px), calc(-50% - 70px)) rotate(-30deg);
+      transform: translate(calc(-50% - 170px), calc(-50% - 70px)) rotate(-30deg);
+
+        @media (max-width: 824px) {
+          transform: translate(calc(-50% - 120px), calc(-50% - 50px)) rotate(-30deg);
+        }
+
+        @media (max-width: 679px) {
+          transform: translate(calc(-50% - 120px), calc(-50% - 25px)) rotate(-30deg);
+        }
+
+        @media (max-width: 539px) {
+          transform: translate(calc(-50% - 90px), calc(-50% - 25px)) rotate(-30deg);
+        }
     }
 
     &--third {
       transform: translate(calc(-50% + 20px), calc(-50% - 140px));
+
+      @media (max-width: 824px) {
+        transform: translate(calc(-50% + 20px), calc(-50% - 110px));
+      }
+
+        @media (max-width: 679px) {
+          transform: translate(calc(-50% + 0px), calc(-50% - 80px))
+        }
     }
 
     &--fourth {
-      transform: translate(calc(-50% + 200px), calc(-50% - 80px)) rotate(30deg);
+      transform: translate(calc(-50% + 210px), calc(-50% - 70px)) rotate(30deg);
+
+      @media (max-width: 824px) {
+        transform: translate(calc(-50% + 160px), calc(-50% - 55px)) rotate(30deg);
+      }
+
+        @media (max-width: 679px) {
+          transform: translate(calc(-50% + 120px), calc(-50% - 30px)) rotate(30deg);
+        }
+
+        @media (max-width: 539px) {
+          transform: translate(calc(-50% + 90px), calc(-50% - 30px)) rotate(30deg);
+        }
     }
 
     &--fifth {
-      transform: translate(calc(-50% + 310px), calc(-50% + 60px)) rotate(50deg);
+      transform: translate(calc(-50% + 320px), calc(-50% + 85px)) rotate(50deg);
+
+        @media (max-width: 824px) {
+          transform: translate(calc(-50% + 260px), calc(-50% + 50px)) rotate(50deg);
+        }
+
+        @media (max-width: 679px) {
+          transform: translate(calc(-50% + 200px), calc(-50% + 50px)) rotate(50deg);
+        }
+
+        @media (max-width: 539px) {
+          transform: translate(calc(-50% + 145px), calc(-50% + 50px)) rotate(50deg);
+        }
     }
   }
 
@@ -115,6 +191,15 @@ import ButtonCta from '@/components/ui/ButtonCta.vue'
       background: radial-gradient(circle, rgba(168, 85, 247, 0.55) 30%, rgba(168, 85, 247, 0) 70%);
       filter: blur(20px);
       transform: translate(-50%, -50%);
+
+        @media (max-width: 539px) {
+          background: none;
+        }
+    }
+
+    @media (max-width: 679px) {
+      left: 46%;
+      transform: translate(-46%, -50%);
     }
 
     img {
